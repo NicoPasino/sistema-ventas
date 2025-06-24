99ba2207181fb7b3b161ac27b4fb8e71a3e50d04
Nicolas Pasino <niko_pasino@hotmail.com>
2025-06-24 13:49:42 -0300

    Ref+Add: actualizar y  obtener.

--- src/components/useItems.js
+++ src/components/useItems.js
@@ -1,42 +1,34 @@
 import { useEffect, useState } from "react";
 
-function useGetItems (itemsDB) {
-  const [items, updateItems] = useState([])
-
+export function useItems({itemsDB}) {
+  const [ items, setItems ] = useState([]);
+  
   const recargarItems = async (items) => {
-    updateItems(await items.obtenerTodos());
+    setItems(await items.obtenerTodos());
   }
 
   useEffect(() => {
     recargarItems(itemsDB);
   }, [itemsDB]);
 
-  return { items, recargarItems }
-}
-
-export function useItems({itemsDB}) {
-  const { items, recargarItems } = useGetItems(itemsDB);
-
-  const agregar = async () => {
-    const nuevo = {
-      Producto: 'Nombre producto',
-      Descripcion: 'Descripción del producto',
-      Cantidad: Math.round(Math.random()*8)+1,
-      Precio: Math.round(Math.random()*90),
-      Fecha: Date.now(),
-      
-      Productos: 'Nombres o ID de productos',
-      Cliente: 'Cliente ' + String(Date.now()).at(-1),
-    };
-    await itemsDB.agregar(nuevo);
+  const agregar = async ({nuevoItem}) => {
+    await itemsDB.agregar(nuevoItem);
+    recargarItems(itemsDB);
+  };
+  const actualizar = async ({nuevoDato}) => {
+    await itemsDB.actualizar(nuevoDato);
+    recargarItems(itemsDB);
+  };
+  const obtenerPorId = async (id) => {
+    await itemsDB.obtenerPorId(id);
     recargarItems(itemsDB);
   };
   const eliminar = async (id) => {
-    const respuestaConfirm = confirm("Realmente quieres eliminar este elemento?")
+    const respuestaConfirm = confirm("Realmente quieres eliminar este elemento?"); // TODO: modal
     if (!respuestaConfirm) return;
     itemsDB.eliminar(id);
     recargarItems(itemsDB);
-  }
+  };
 
-  return { items, agregar, eliminar, recargarItems }
+  return { items, agregar, actualizar, obtenerPorId, eliminar, recargarItems }
 }
